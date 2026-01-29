388177345a4f62cd528cda22c86018c85b6fbede
Yonas <116105802+yonas255@users.noreply.github.com>
2026-01-29 12:49:08 +0000

    adding permssions and completion message

--- script.sh
+++ script.sh
@@ -1,4 +1,3 @@
-cat > script.sh <<'EOF'
 #!/bin/bash
 # This script creates two folders: fol_1 and fol_2
 
@@ -16,6 +15,11 @@ touch fol_1/1_1.txt fol_1/1_2.txt fol_1/1_3.txt
 # Step 5: Create files in fol_2
 touch fol_2/2_1.txt fol_2/2_2.txt fol_2/2_3.txt
 
-echo "Folders and files created."
-EOF
+# step 10: _1.txt and _3.txt -> owner read/write only
+chmod 600 fol_1/*_1.txt fol_1/*_3.txt fol_2/*_1.txt fol_2/*_3.txt
 
+# Step 11: _2.txt -> everyone read/write/execute
+chmod 777 fol_1/*_2.txt fol_2/*_2.txt
+
+# Step 17: completion message
+echo "Job completed"
